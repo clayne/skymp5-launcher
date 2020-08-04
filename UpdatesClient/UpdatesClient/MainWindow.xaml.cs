b40cc18aeff812a60fb8c938bf221943f2a3b0e3
Keidran <keid.keyla@gmail.com>
2020-08-05 02:45:16 +0300

    Base logic without web connection

--- UpdatesClient/UpdatesClient/MainWindow.xaml.cs
+++ UpdatesClient/UpdatesClient/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -21,21 +23,71 @@ namespace UpdatesClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Check file "SkyrimSE.exe"
+        //Base color: #FF04D9FF
+        //War color: #FFFF7604
+        //Er color: #FFFF0404
+
         public MainWindow()
         {
-            try
-            {
-                InitializeComponent();
-                TitleWindow.MouseLeftButtonDown += (s, e) => DragMove();
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show(e.ToString());
-            }
+            InitializeComponent();
+            TitleWindow.MouseLeftButtonDown += (s, e) => DragMove();
+
+            wind.Loaded += delegate {
+                if (string.IsNullOrEmpty(Properties.Settings.Default.PathToSkyrim))
+                {
+                    using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+                    {
+                        System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+
+                        if (result == System.Windows.Forms.DialogResult.OK && File.Exists(dialog.SelectedPath + "\\SkyrimSE.exe"))
+                        {
+                            Properties.Settings.Default.PathToSkyrim = dialog.SelectedPath;
+                            Properties.Settings.Default.Version = "0.0.0.0";
+                            Properties.Settings.Default.Save();
+                        }
+                        else { Application.Current.Shutdown(); }
+                    }
+                }
+
+                BrushConverter converter = new BrushConverter();
+                MainBtn.ColorBar = (Brush)converter.ConvertFrom("#FF04D9FF");
+                CheckClient();
+            };
+
         }
+
+        private async void CheckClient()
+        {
+            MainBtn.IsIndeterminate = true;
+
+            await Task.Delay(1000);
+
+            //CheckUpdate
+
+            MainBtn.StatusText = "ИГРАТЬ";
+            MainBtn.IsIndeterminate = false;
+            MainBtn.IsDisabled = false;
+        }
+
 
         private void ImageButton_Click(object sender, EventArgs e)
         {
+            Application.Current.Shutdown();
+        }
+
+        private void MainBtn_Click(object sender, EventArgs e)
+        {
+            //Play
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+
+            startInfo.FileName = $"{Properties.Settings.Default.PathToSkyrim}\\SkyrimSE.exe";
+            startInfo.Arguments = $"--UUID Launcher --Session TEST";
+            startInfo.WorkingDirectory = $"{Properties.Settings.Default.PathToSkyrim}\\";
+
+            process.StartInfo = startInfo;
+            process.Start();
             Close();
         }
     }
